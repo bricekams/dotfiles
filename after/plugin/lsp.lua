44fbed9ad42c3ea32d99a2dda59e3d7a94f8359b
Brice Kamhoua <kamhoua.k.brice@gmail.com>
2023-11-30 17:44:21 +0100

    cmp.lua

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -1,18 +1,5 @@
 local lsp_zero = require('lsp-zero')
 
--- cmp
-
-local cmp = require('cmp')
-local cmp_action = lsp_zero.cmp_action()
-
-cmp.setup({
-  mapping = cmp.mapping.preset.insert({
-    ['<Tab>'] = cmp_action.tab_complete(),
-    ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  })
-})
-
 lsp_zero.on_attach(function(client, bufnr)
   -- see :help lsp-zero-keybindings
   -- to learn the available actions
